e06ea0f009141c9a4637361d9a2c992a4e8b86a3
Luisgr <luijo915@hotmail.com>
2024-05-27 18:03:43 +0100

    Agrego imagen en pagina home

--- src/Page/home.jsx
+++ src/Page/home.jsx
@@ -1,9 +1,15 @@
 import "../style/home.css"
+import Image from 'react-bootstrap/Image';
 
 export const Home = () => {
   return (
-    <div className="home" >
-        <h1>Hola Esta es tu pagina Home</h1>
+    <div className="container-fluid">
+      <div>
+      <img src="https://d375139ucebi94.cloudfront.net/region2/es/40058/biz_photo/ed4f11097acb4662b2dbe6defc00dd-peluqueria-organica-las-canas--biz-photo-b70eb0719dbe475396cdd6a9e3c76f-booksy.jpeg?size=640x427"
+       className="img-home" 
+       alt="Imagen del salón"/>
+      </div>
+        
     </div>
   )
 }
